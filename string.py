ac763d5eeada3fb41f49e77a291b6781ae4c7fd8
coderofficer007 <124818909+coderofficer007@users.noreply.github.com>
2023-02-10 01:26:35 +0530

    Updates

---
+++ string.py
@@ -0,0 +1,13 @@
+#greeting ="good morning  "
+#name="prayas"
+#print(type(name))
+# Concatenating two strings 
+#a = greeting+name
+#print(a)
+#name="prayas"
+#print(name[3])
+#print(name[-4:-1])
+name="1234567890"
+a=name[0::2]
+print(a)
+
